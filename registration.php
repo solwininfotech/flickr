739f6cf18ead8d571c419d1fc8647a09447873fb
solwininfotech <info@solwininfotech.com>
2020-05-28 18:27:59 +0530

    Initial commit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Flickr Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_Flickr
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Flickr',
+    __DIR__
+);
